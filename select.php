2253698c04558181836c77d7146121d548410206
Juan Forero <juanf212@gmail.com>
2018-06-15 09:50:32 -0500

    [refactor] cambio de ciclo y funcion asociativa

--- select.php
+++ select.php
@@ -3,10 +3,9 @@
 
     $sql = "SELECT*FROM equipos";
     $ResultadoDeLaConsulta = mysqli_query($cox,$sql);
-    $equipos = mysqli_fetch_all($ResultadoDeLaConsulta);
 
-    foreach ($equipos as $equipo) {
-        echo $equipo[1]. '<br>';
+    while ($equipo = mysqli_fetch_assoc($ResultadoDeLaConsulta)) {
+        echo $equipo['nombre']. '<br>';
     }
 ?>
 
