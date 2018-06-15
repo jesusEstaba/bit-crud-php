fabe5a3ca58c2e1b03905d8fce0c21a04a2e2da3
Jesus Estaba <jeec.estaba@gmail.com>
2018-06-15 09:31:38 -0500

    solo tiene que imprimir con un ciclo

--- select.php
+++ select.php
@@ -1,27 +1,12 @@
 <?php
-//select.php (for)
-
     require('conexion.php');
 
-    $sql="SELECT*FROM equipos";
+    $sql = "SELECT*FROM equipos";
+    $ResultadoDeLaConsulta = mysqli_query($cox,$sql);
+    $equipos = mysqli_fetch_all($ResultadoDeLaConsulta);
 
-    $ResultadoDeLaConsulta=mysqli_query($cox,$sql);
-
-    $equipos= mysqli_fetch_all($ResultadoDeLaConsulta);
-
-    #hacerlo con for
-    for($i=0;$i<count($equipos);$i++) {
-        
-        echo $equipos[$i][1]. '<br>';
-    }
-    #hacerlo con foreach
     foreach ($equipos as $equipo) {
-        
         echo $equipo[1]. '<br>';
     }
-    #llamado directo al arreglo
-    echo $equipos [0][1];
-
-
 ?>
 
