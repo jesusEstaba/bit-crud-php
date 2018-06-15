428fdfb96760ec1c172c6d3fbb8778c099623733
Jesus Estaba <jeec.estaba@gmail.com>
2018-06-15 08:37:34 -0500

    standar PSR-2

--- update.php
+++ update.php
@@ -1,14 +1,9 @@
 <?php
-//update.php
+    require('conexion.php');
 
-require('conexion.php');
+    $equipo = $_GET['equipo'];
+    $id = $_GET['id'];
 
-$equipo=$_GET['equipo'];
-$id=$_GET['id'];
-
-    $sql="UPDATE equipos SET nombre='$equipo' WHERE id='$id'";
-    mysqli_query($cox,$sql);
-
-
-
+    $sql = "UPDATE equipos SET nombre='$equipo' WHERE id='$id'";
+    mysqli_query($cox, $sql);
 ?>
